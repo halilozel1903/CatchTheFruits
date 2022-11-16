ac0c4b7bc65a44ca256ada63b8e3c18906d83157
Halil Özel <halilozel1903@gmail.com>
2022-11-17 01:04:59 +0300

    Code reformat made.

--- app/src/main/java/com/halil/ozel/catchthefruits/MainActivity.kt
+++ app/src/main/java/com/halil/ozel/catchthefruits/MainActivity.kt
@@ -103,7 +103,6 @@ class MainActivity : AppCompatActivity() {
                         }
                         finish()
                     }
-
                  dialog.create().apply {
                     show()
                 }
